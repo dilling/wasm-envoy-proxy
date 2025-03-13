739f8c68251abdcc1aab11bbe0df1cfa40bf9dc5
David Illing <dilling123@gmail.com>
2025-03-13 17:43:58 -0600

    fix service + client connection

--- hack/client/main.py
+++ hack/client/main.py
@@ -1,17 +1,18 @@
 from gen.potatoService import PotatoService
 
-from thrift.transport import TSocket
+from thrift.transport import THttpClient
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 import time
 
 def main():
-    transport = TSocket.TSocket('localhost', 9090)
+    transport = THttpClient.THttpClient('http://service:9090')
     transport = TTransport.TBufferedTransport(transport)
     protocol = TBinaryProtocol.TBinaryProtocol(transport)
     client = PotatoService.Client(protocol)
 
     while True:
+        print("Sending Request...")
         try:
             transport.open()
             spud = client.getSpud()
--- hack/service/app.py
+++ hack/service/app.py
@@ -1,25 +1,24 @@
 from gen.potatoService import PotatoService
 
-from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
-
-from thrift.server import TServer
+from thrift.server import THttpServer
 
 class PotatoServiceHandler(PotatoService.Iface):
     def getSpud(self):
         return "🥔"
 
-
 if __name__ == '__main__':
-    print('POTATO SERVICE')
     handler = PotatoServiceHandler()
     processor = PotatoService.Processor(handler)
-    transport = TSocket.TServerSocket(port=9090)
     tfactory = TTransport.TBufferedTransportFactory()
     pfactory = TBinaryProtocol.TBinaryProtocolFactory()
 
-    server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+    server = THttpServer.THttpServer(
+        processor,
+        ('', 9090),
+        pfactory
+    )
 
     print('Starting the server...')
     server.serve()
