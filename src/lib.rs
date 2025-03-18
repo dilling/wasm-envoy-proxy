2027fa5e06f1fdb3e4dfa62b4c5dea780eaf6c9c
David Illing <dilling123@gmail.com>
2025-03-17 21:19:10 -0400

    parse jwt

--- src/lib.rs
+++ src/lib.rs
@@ -13,24 +13,67 @@
 // limitations under the License.
 
 use std::time::Duration;
+use jwt_simple::prelude::RS256PublicKey;
 use log;
+use serde::Deserialize;
+use serde_json::from_slice;
 
 use proxy_wasm::{
     traits::{Context, HttpContext, RootContext}, types::{Action, ContextType, LogLevel}
 };
 
-const PUBLIC_KEY_CACHE_KEY: &str = "public_key";
 const PUBLIC_KEY_REFRESH_INTERVAL: Duration = Duration::from_secs(3);
 
 proxy_wasm::main! {{
     proxy_wasm::set_log_level(LogLevel::Trace);
-    proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> { Box::new(RootHandler) });
-    // proxy_wasm::set_http_context(|_context_id, _root_context_id| -> Box<dyn HttpContext> {
-    //     Box::new(HttpHandler {})
-    // })
+    proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> { Box::new(RootHandler::default()) });
 }}
 
-struct RootHandler;
+#[derive(Debug, Clone, Deserialize)]
+struct Jwks {
+    keys: Vec<Jwk>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct Jwk {
+    kty: String,
+    kid: String,
+    // use_: String,
+    alg: String,
+    n: String,
+    e: String,
+}
+
+#[derive(Default)]
+struct RootHandler {
+    public_key: Option<RS256PublicKey>,
+}
+
+impl RootHandler {
+    fn handle_get_token_res(&mut self, jwks: Vec<u8>) {
+        let jwks: Jwks = match from_slice(&jwks) {
+            Ok(jwks) => jwks,
+            Err(e) => {
+                log::error!("Failed to parse JWKS: {:?}", e);
+                return;
+            }
+        };
+
+        let pubkey_comps = match jwks.keys.get(0) {
+            Some(key) => key,
+            None => {
+                log::error!("No keys found in JWKS");
+                return;
+            }
+        };
+
+        log::info!("Public Key: {:?}", pubkey_comps);
+
+        let key = RS256PublicKey::from_components(&pubkey_comps.n.as_bytes(), &pubkey_comps.e.as_bytes()).unwrap();
+
+        self.public_key = Some(key);
+    }
+}
 
 impl RootContext for RootHandler {
     fn create_http_context(&self, _context_id: u32) -> Option<Box<dyn HttpContext>> {
@@ -43,19 +86,17 @@ impl RootContext for RootHandler {
 
     fn on_configure(&mut self, _plugin_configuration_size: usize) -> bool {
         self.set_tick_period(PUBLIC_KEY_REFRESH_INTERVAL);
-        log::debug!("on_configure");
+        log::info!("on_configure");
         true
     }
 
-    
-
     fn on_tick(&mut self) {
-        match self.get_shared_data(PUBLIC_KEY_CACHE_KEY) {
-            (Some(_), _) => {
+        match self.public_key {
+            Some(_) => {
                 log::info!("Public Key cached, skipping fetch");
                 return;
             },
-            (None, _) => log::info!("fetching public key"),
+            None => log::info!("fetching public key"),
         }
 
         let _ = self.dispatch_http_call(
@@ -70,7 +111,7 @@ impl RootContext for RootHandler {
             Duration::from_secs(1)
         ).inspect_err( |e| {
             log::warn!("dispatch_http_call failed, retrying: {:?}", e);
-        }).unwrap();
+        });
     }
 }
 
@@ -96,6 +137,8 @@ impl Context for RootHandler {
         };
 
         log::info!("{}", String::from_utf8(body.clone()).unwrap());
+
+        self.handle_get_token_res(body);
         
         // self.set_shared_data(PUBLIC_KEY_CACHE_KEY, Some(body.as_slice()), None);
     }
